6c1ea55517eb4de07e8e0675b33349b29a1e4173
Tretorhate <tretorhate@gmail.com>
2025-02-20 17:56:20 +0500

    little post alteration

--- lib/models/Post.ts
+++ lib/models/Post.ts
@@ -1,15 +1,13 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema(
   {
     title: String,
     content: String,
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tags: [String],
   },
-  { timestamps: true },
-)
+  { timestamps: true }
+);
 
-export default mongoose.models.Post || mongoose.model("Post", PostSchema)
-
+export default mongoose.models.Post || mongoose.model("Post", PostSchema);
