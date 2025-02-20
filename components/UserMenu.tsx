32d7870587d1224873da1d7ba9fe1e47fb511b4c
Tretorhate <tretorhate@gmail.com>
2025-02-20 17:41:32 +0500

    small fix

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -16,11 +16,6 @@ export function UserMenu() {
     return (
       <>
         <NavigationMenuItem>
-          <NavigationMenuLink href="/create-post" className="px-4 py-2">
-            Create Post
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
           <Button onClick={() => signOut()} variant="ghost">
             Sign Out
           </Button>
